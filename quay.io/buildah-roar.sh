85277c1dd48ac310de8bf74bfbcd6cd788ce5f47
Brent Laster <bcl@nclasters.org>
2022-11-20 15:50:34 -0500

    update buildah scripts

--- quay.io/buildah-roar.sh
+++ quay.io/buildah-roar.sh
@@ -5,7 +5,7 @@ buildah copy $ctr1 docker-entrypoint-initdb.d /docker-entrypoint-initdb.d/
 buildah config --entrypoint '["/entrypoint.sh","mysqld"]' $ctr1
 buildah commit $ctr1 roar-db:1.0.1
 
-ctr2=$(buildah from docker.io/tomcat:7.0.65-jre7)
+ctr2=$(buildah from quay.io/bclaster/tomcat:7.0.65-jre7)
 buildah copy $ctr2 $1 $CATALINA_HOME/webapps/roar.war
 buildah config --cmd '["catalina.sh", "run"]' $ctr2
 buildah commit $ctr2 roar-web:1.0.1
